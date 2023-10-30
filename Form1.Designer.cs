23aae94a8682d513988fa1e4bb504d81034c300c
ylishu <654738266@qq.com>
2023-10-30 21:11:39 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace addressBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackgroundImageTiled = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Font = new System.Drawing.Font("宋体", 12F);
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 32);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 198);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "序号";
+            this.columnHeader1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Tag = "";
+            this.columnHeader2.Text = "姓名";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "电话";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 180;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "地址";
+            this.columnHeader4.Width = 300;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label1.Location = new System.Drawing.Point(340, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "联系人列表";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F);
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(12, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "输入信息";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(87, 274);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(199, 25);
+            this.txtName.TabIndex = 3;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(87, 319);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(199, 25);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(87, 365);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(199, 61);
+            this.txtAddress.TabIndex = 5;
+            this.txtAddress.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F);
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(12, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "姓名：";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F);
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(12, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "电话：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 12F);
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(12, 367);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "地址：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 12F);
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(474, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "联系人操作";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 12F);
+            this.button3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button3.Location = new System.Drawing.Point(478, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 42);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "添加联系人信息";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Font = new System.Drawing.Font("宋体", 12F);
+            this.buttonEdit.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonEdit.Location = new System.Drawing.Point(478, 331);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(186, 40);
+            this.buttonEdit.TabIndex = 13;
+            this.buttonEdit.Text = "修改选中信息";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Font = new System.Drawing.Font("宋体", 12F);
+            this.buttonDel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonDel.Location = new System.Drawing.Point(478, 386);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(186, 40);
+            this.buttonDel.TabIndex = 14;
+            this.buttonDel.Text = "删除选中信息";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            this.buttonDel.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 438);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "个人通讯录系统_郭豪";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+
+namespace addressBook
+{
+    public partial class Form1 : Form
+    {
+        //数据库连接字符串
+        private static string connString = "server=127.0.0.1;port=3306;database=softwaretest3;username=root;password=gh200262...; SslMode=none;";
+        MySqlConnection conn = new MySqlConnection(connString);
+
+        private bool isAdd;
+        private int Fid;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.getInfo();
+            //try {
+            //   conn.Open();
+            //    MessageBox.Show("连接成功");
+            //} catch (Exception ex){
+            //    MessageBox.Show(ex.Message);
+            //}
+        }
+
+        private void getInfo()
+        {
+            String sql = "select person_id, person_name, person_phone, person_address from person_list";
+            //MessageBox.Show(sql);
+            MySqlConnection cnn = new MySqlConnection(connString);
+            cnn.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, cnn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            try
+            {
+                this.listView1.Items.Clear();
+                while (reader.Read())
+                {
+                    string[] subItems = new string[]{
+                   reader.GetInt32(0).ToString(),
+                   reader.GetString(1),
+                    reader.GetString(2),
+                    reader.GetString(3)};
+                    this.listView1.Items.Add(new ListViewItem(subItems));
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (listView1.SelectedItems.Count == 0)
+                return;
+            if (this.listView1.FocusedItem == null)
+                return;
+            ListViewItem item = listView1.SelectedItems[0];
+            txtName.Text = item.SubItems[1].Text;
+            txtPhone.Text = item.SubItems[2].Text;
+            txtAddress.Text = item.SubItems[3].Text;
+            this.Fid = int.Parse(item.SubItems[0].Text);
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void enableInfo()
+        {
+            this.clearInfo();
+            this.getInfo();
+            this.txtAddress.Enabled = true;
+            this.txtName.Enabled = true;
+        }
+        private void clearInfo()
+        {
+            this.listView1.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.isAdd)
+            {
+                try
+                {
+                    addInfo(this.txtName.Text, this.txtPhone.Text, this.txtAddress.Text);
+                    MessageBox.Show("成功添加联系人。");
+    		        this.getInfo();
+                    this.enableInfo();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    changeInfo(Fid, this.txtName.Text, this.txtPhone.Text, this.txtAddress.Text);
+                    MessageBox.Show("成功修改联系人信息。");
+	                this.getInfo();
+                    this.enableInfo();
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //this.enableInfo();
+            //this.buttonEdit.Enabled = false;
+            //this.buttonDel.Enabled = false;
+            //this.isAdd = true;
+            try
+            {
+                addInfo(this.txtName.Text, this.txtPhone.Text, this.txtAddress.Text);
+                MessageBox.Show("成功添加联系人。");
+                this.getInfo();
+                this.enableInfo();
+                this.txtName.Text = "";
+                this.txtPhone.Text = "";
+                this.txtAddress.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //添加
+        public void addInfo(string name, string phone, string address)
+        {
+            string sql = "insert into person_list(person_name,person_phone,person_address) values("+"'"+name+"'"+","+"'"+phone+"'"+","+"'"+address+"'"+")";
+
+            MySqlConnection conn = new MySqlConnection(connString);
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        //修改
+        public void changeInfo(int id, string name, string phone, string address)
+        {
+            string sql = "update person_list set person_name='" + name + "',person_phone='" + phone + "',person_address = '"+address+"'";
+            sql += "where person_id=" + id;
+            MySqlConnection conn = new MySqlConnection(connString);
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private int getSelectID()
+        {
+            return this.Fid;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                changeInfo(Fid, this.txtName.Text, this.txtPhone.Text, this.txtAddress.Text);
+                MessageBox.Show("成功修改联系人信息。");
+                this.getInfo();
+                this.enableInfo();
+                this.txtName.Text = "";
+                this.txtPhone.Text = "";
+                this.txtAddress.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //删除
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int infoID = this.getSelectID();
+            if (infoID == 0 || infoID == null)
+            {
+                MessageBox.Show("请先选中联系人信息！");
+                return;
+            }
+            try
+            {
+                string sql = "delete from person_list where person_id=" + infoID.ToString();
+                MySqlConnection conn = new MySqlConnection(connString);
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("删除联系人成功！");
+                this.getInfo();
+                this.enableInfo();
+                this.txtName.Text = "";
+                this.txtPhone.Text = "";
+                this.txtAddress.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
